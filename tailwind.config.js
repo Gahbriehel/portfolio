994af5fa03cdaebe2c0baa7fad32903ad33f2df1
gahbriehel <babatise002@gmail.com>
2025-11-24 23:07:57 +0100

    add tailwind gradient combinations support in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
+  safelist: [
+    'bg-gradient-to-r',
+    'from-blue-500',
+    'to-purple-500',
+    'from-green-500',
+    'to-teal-500',
+    'from-orange-500',
+    'to-red-500',
+
+    {
+      pattern: /bg-gradient-to-r/,
+    },
+    {
+      pattern: /from-(blue|green|orange|red|purple|teal|emerald|cyan|indigo)-[45]00/,
+      variants: ['hover', 'dark'],
+    },
+    {
+      pattern: /to-(purple|teal|red|emerald|cyan|indigo)-[45]00/,
+      variants: ['hover', 'dark'],
+    },
+  ],
   theme: {
     extend: {
       fontFamily: {
